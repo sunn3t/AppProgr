e784e5729e7f242ba9b0b8700d9bd7146baa5aac
shulepa ivan <154735543+sunn3t@users.noreply.github.com>
2024-11-06 16:24:22 +0200

    Update T20.1_12.py

--- hw_3/T20.1_12.py
+++ hw_3/T20.1_12.py
@@ -5,39 +5,27 @@ n_max = 1000
 n = np.arange(1, n_max + 1)
 an = n**(1/n)
 
-# Гіпотеза: границя b
 b = 1
-
-# Задане epsilon
 epsilon = 0.05
 
-# Перевірка існування такого k, для якого |a_m - b| < epsilon для всіх m > k
 condition = np.abs(an - b) < epsilon
-k = np.argmax(condition)  # Перший індекс, починаючи з якого виконується умова
+k = np.argmax(condition)
 
-# Побудова графіку
 plt.figure(figsize=(10, 6))
 
-# Графік послідовності a_n
 plt.plot(n, an, label='$a_n = n^{1/n}$', color='blue')
-
-# Пряма y = b
 plt.axhline(y=b, color='green', linestyle='--', label='$y = b = 1$')
-
-# Смуга (b - epsilon, b + epsilon)
 plt.fill_between(n, b - epsilon, b + epsilon, color='yellow', alpha=0.3, label='b - epsilon < a_n < b + epsilon')
 
-# Позначення точки k
 plt.scatter([k], [an[k]], color='red', zorder=5)
 plt.axvline(x=k, color='red', linestyle=':', label=f'$k = {k}$')
 
-# Налаштування графіка
+
 plt.xlabel('$n$')
 plt.ylabel('$a_n$')
 plt.title('Послідовність $a_n = n^{1/n}$ та її границя')
 plt.legend()
 plt.grid()
-plt.ylim(0.9, 1.1)  # Масштаб осі y для ілюстрації границі
+plt.ylim(0.9, 1.1)
 
-# Показати графік
 plt.show()
