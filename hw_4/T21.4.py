0c4692d8334c38dcca25da284ee79831dbcd9860
shulepa ivan <154735543+sunn3t@users.noreply.github.com>
2024-11-13 04:29:02 +0200

    Update  code

--- hw_4/T21.4.py
+++ hw_4/T21.4.py
@@ -7,24 +7,15 @@ output_file = 'output.txt'
 with open(input_file, 'r', encoding='utf-8') as file:
     text = file.read()
 
-# Регулярні вирази для пошуку дат і підкреслень
-date_pattern = r'\b\d{2}\.\d{2}\.\d{4}\b'
-underscore_pattern = r'_{2}\._{2}\._{4}'
+date = r'\b\d{2}\.\d{2}\.\d{4}\b'
+underscore_p = r'_{2}\._{2}\._{4}'
 
-# Знаходимо всі дати у тексті
-dates_found = re.findall(date_pattern, text)
-
-# Поточна дата у форматі dd.mm.yyyy
+dates_found = re.findall(date, text)
 current_date = datetime.now().strftime('%d.%m.%Y')
-
-# Замінюємо підкреслення на поточну дату
-updated_text = re.sub(underscore_pattern, current_date, text)
-
-# Зберігаємо оновлений текст у файл
+updated_text = re.sub(underscore_p, current_date, text)
 with open(output_file, 'w', encoding='utf-8') as file:
     file.write(updated_text)
 
-# Виводимо знайдені дати
 print("Знайдені дати у тексті:")
 for date in dates_found:
     print(date)
